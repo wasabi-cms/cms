33a49386575f4b532e41ecd323b29851e1cc65b2
frankfoerster <github@frankfoerster.com>
2015-08-03 07:34:42 +0200

    add Routes backbone view

--- src/Assets/js/cms.js
+++ src/Assets/js/cms.js
@@ -1,11 +1,19 @@
 define(function(require) {
   var $ = require('jquery');
+  var WS = require('wasabi');
+  var Routes = require('wasabi.cms.package/views/Routes');
 
   return {
     initialize: function() {
+      WS.Cms = {
+        views: {}
+      };
       if (DEBUG) {
         console.info('Wasabi/Cms initialized.');
       }
+    },
+    routes: function() {
+      WS.Cms.views.routes = WS.createView(Routes);
     }
   }
 });
