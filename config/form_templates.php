a8b54a9cd3e6362dcf27bae5ee0986a642d8917a
frankfoerster <github@frankfoerster.com>
2015-08-10 20:13:15 +0200

    add Wasabi/Cms.form_templates

---
+++ config/form_templates.php
@@ -0,0 +1,12 @@
+<?php
+return [
+    'formStart' => '<form {{attrs}} novalidate>',
+    'label' => '<label {{attrs}}>{{text}}{{info}}</label>',
+    'input' => '<input type="{{type}}" name="{{name}}"{{attrs}}>{{info}}',
+    'inputContainer' => '<div class="form-row form-row-{{type}}{{required}}">{{content}}</div>',
+    'inputContainerError' => '<div class="form-row form-row-{{type}}{{required}} error">{{content}}</div>',
+    'formGroup' => '{{label}}{{input}}{{error}}',
+    'section' => '<div class="form-section"><div class="form-section-title">{{title}}</div><div class="form-section-description">{{description}}</div></div>',
+    'checkboxFormGroup' => '<label>{{formRowLabel}}</label><div class="field">{{label}}</div>',
+    'select' => '<select name="{{name}}"{{attrs}}>{{content}}</select>{{info}}'
+];
