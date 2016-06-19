0103a3156aba82bf035f845335d4f1a00574f620
frankfoerster <github@frankfoerster.com>
2016-06-19 19:13:48 +0200

    refactor MetaHelper and provide support for all typical meta tags

--- src/View/Helper/MetaHelper.php
+++ src/View/Helper/MetaHelper.php
@@ -1,73 +1,362 @@
 <?php
 /**
- *
- * PHP 5
+ * Wasabi CMS
+ * Copyright (c) Frank Förster (http://frankfoerster.com)
  *
  * Licensed under The MIT License
- * Redistributions of files must retain the below copyright notice.
+ * For full copyright and license information, please see the LICENSE.txt
+ * Redistributions of files must retain the above copyright notice.
  *
  * @copyright     Copyright (c) Frank Förster (http://frankfoerster.com)
- * @link          http://github.com/frankfoerster/wasabi
- * @package       Wasabi
- * @subpackage    Wasabi.Plugin.Core.View.Helper
- * @license       MIT License (http://www.opensource.org/licenses/mit-license.php)
+ * @license       http://www.opensource.org/licenses/mit-license.php MIT License
  */
-
 namespace Wasabi\Cms\View\Helper;
 
+use Cake\Core\Configure;
+use Cake\Routing\Router;
 use Cake\View\Helper;
+use Wasabi\Cms\Model\Entity\Page;
+use Wasabi\Cms\Model\Entity\PublishableEntity;
+use Wasabi\Cms\WasabiCms;
+use Wasabi\Core\Wasabi;
 
-class MetaHelper extends Helper {
+/**
+ * Class MetaHelper
+ *
+ * @property \Cake\View\Helper\UrlHelper Url
+ */
+class MetaHelper extends Helper
+{
+    const ATTR_NAME = 'name';
+    const ATTR_PROP = 'propery';
 
-	public function meta($attributes) {
-		$meta = array()
-			+ $this->_general($attributes)
-			+ $this->_openGraph($attributes)
-			+ $this->_twitter($attributes)
-			+ $this->_geo($attributes);
+    /**
+     * Helpers used by this helper.
+     *
+     * @var array
+     */
+    public $helpers = [
+        'Url'
+    ];
 
-		$out = '';
-		foreach ($meta as $property => $content) {
-			$out .= '<meta name="' . $property . '" content="' . $content . '">';
-		}
+	/**
+     * Render the viewport meta tag.
+     *
+     * @param string $content viewport content
+     * @return string
+     */
+    public function viewport($content)
+    {
+        return $this->_render(self::ATTR_NAME, __FUNCTION__, $content);
+    }
 
-		return $out;
-	}
+    /**
+     * Render the page title.
+     *
+     * @param Page $page The page.
+     * @return string
+     */
+    public function title($page)
+    {
+        return '<title>' . $this->_getTitle($page) . '</title>';
+    }
 
-	protected function _general($attributes) {
-		$allowed = array('keywords', 'description', 'robots', 'viewport');
+    /**
+     * Render the description meta tag.
+     *
+     * @param string $content description content
+     * @return string
+     */
+    public function description($content)
+    {
+        return $this->_render(self::ATTR_NAME, __FUNCTION__, $content);
+    }
 
-		return $this->_processAttributes($allowed, $attributes);
-	}
+    /**
+     * Render the application-name meta tag.
+     *
+     * @param string $content application-name content
+     * @return string
+     */
+    public function applicationName($content)
+    {
+        return $this->_render(self::ATTR_NAME, 'application-name', $content);
+    }
 
-	protected function _openGraph($attributes) {
-		$allowed = array('og:sitename', 'og:title', 'og:description', 'og:image', 'og:image:type', 'og:image:width', 'og:image:height', 'og:url', 'og:locale', 'og:type','og:updated_time');
+    /**
+     * Render the robots meta tag.
+     *
+     * @param PublishableEntity $entity The entity, e.g. Page or Post.
+     * @return string
+     */
+    public function robots($entity)
+    {
+        $robots = [];
 
-		return $this->_processAttributes($allowed, $attributes);
-	}
+        $index = (bool)Configure::read('Settings.Cms.SEO.meta-robots-index');
+        if ($entity->meta_robots_index !== null) {
+            $index = $entity->meta_robots_index;
+        }
+        $robots[] = ($index) ? 'index' : 'noindex';
 
-	protected function _twitter($attributes) {
-		$allowed = array('twitter:card', 'twitter:image', 'twitter:site', 'twitter:creator');
+        $follow = (bool)Configure::read('Settings.Cms.SEO.meta-robots-follow');
+        if ($entity->meta_robots_follow !== null) {
+            $follow = $entity->meta_robots_follow;
+        }
+        $robots[] = ($follow) ? 'follow' : 'nofollow';
 
-		return $this->_processAttributes($allowed, $attributes);
-	}
+        $noodp = (bool)Configure::read('Settings.Cms.SEO.meta-robots-noodp') || !empty($entity->meta_description);
+        if ($noodp) {
+            $robots[] = 'noodp';
+        }
 
-	protected function _geo($attributes) {
-		$allowed = array('icbm', 'geo.placename', 'geo.region');
+        return $this->_render(self::ATTR_NAME, __FUNCTION__, join(',', $robots));
+    }
 
-		return $this->_processAttributes($allowed, $attributes);
-	}
+    /**
+     * Render all og meta tags.
+     *
+     * @param PublishableEntity $entity The entity, e.g. Page or Post.
+     * @return string
+     */
+    public function opengraph($entity)
+    {
+        $attributes = [
+            'og:locale' => Wasabi::contentLanguage()->lang,
+            'og:type' => $entity->attributes['og:type'] ?? 'article',
+            'og:title' => $this->_getTitle($entity),
+            'og:description' => $entity->attributes['og:description'] ?? $entity->meta_description,
+            'og:url' => Router::url(null, true),
+            'og:site_name' => Configure::read('Settings.Cms.SEO.application-name')
+        ];
 
-	protected function _processAttributes($allowed, $attributes) {
-		$result = array();
+        if ($entity->displayMetaDates) {
+            $attributes['og:updated_time'] = $entity->modified->format('c');
+        }
 
-		foreach($allowed as $property) {
-			if (isset($attributes[$property]) && $attributes[$property] !== '') {
-				$result[$property] = $attributes[$property];
-			}
-		}
+        $fbPageId = Configure::read('Settings.Cms.SEO.Social.facebook_page_id');
+        if ($fbPageId !== null) {
+            $attributes['fb:admins'] = $fbPageId;
+        }
 
-		return $result;
-	}
+        $out = '';
+        foreach ($attributes as $property => $content) {
+            if ($property === 'og:title') {
+                $out .= $this->_render(self::ATTR_PROP, $property, $content, self::ATTR_NAME, 'dcterms.title');
+            } else {
+                $out .= $this->_render(self::ATTR_PROP, $property, $content);
+            }
+        }
 
+        return $out;
+    }
+
+    /**
+     * Render all twitter tags.
+     *
+     * @param PublishableEntity $entity The entity, e.g. Page or Post.
+     * @return string
+     */
+    public function twitter($entity)
+    {
+        $twitterName = Configure::read('Settings.Cms.SEO.Social.twitter_username');
+        if ($twitterName !== null) {
+            $twitterName = '@' . $twitterName;
+        }
+
+        $attributes = [
+            'twitter:card' => 'summary',
+            'twitter:title' => $this->_getTitle($entity),
+            'twitter:description' => $entity->attributes['og:description'] ?? $entity->meta_description,
+            'twitter:image' => $this->_getSocialImage($entity),
+            'twitter:site' => $twitterName,
+            'twitter:creator' => $twitterName,
+        ];
+
+        $out = '';
+        foreach ($attributes as $property => $content) {
+            $out .= $this->_render(self::ATTR_NAME, $property, $content);
+        }
+
+        return $out;
+    }
+
+    /**
+     * Render all article meta tags.
+     *
+     * @param PublishableEntity $entity The entity, e.g. Page or Post.
+     * @return string
+     */
+    public function article($entity)
+    {
+        if (!$entity->displayArticleMetaTags) {
+            return '';
+        }
+
+        $attributes = [
+            'article:publisher' => Configure::read('Settings.Cms.SEO.Social.facebook_url'),
+            'article:tag' => '',
+            'article:section' => ''
+        ];
+
+        if ($entity->displayMetaDates) {
+            $attributes['article:published_time'] = ($entity->published !== null) ? $entity->published->format('c') : null;
+            $attributes['article:modified_time'] = $entity->modified->format('c');
+        }
+
+        $out = '';
+        foreach ($attributes as $property => $content) {
+            $out .= $this->_render(self::ATTR_PROP, $property, $content);
+        }
+
+        return $out;
+    }
+
+    /**
+     * Render the DC.date.issued meta tag.
+     *
+     * @param PublishableEntity $entity The entity, e.g. Page or Post.
+     * @return string
+     */
+    public function dcDateIssued($entity)
+    {
+        if (!$entity->displayMetaDates) {
+            return '';
+        }
+        $published = ($entity->published !== null) ? $entity->published->format('c') : null;
+        return $this->_render(self::ATTR_PROP, 'DC.date.issued', $published);
+    }
+
+    /**
+     * Render the google site verification meta tag.
+     *
+     * @return string
+     */
+    public function googleSiteVerification()
+    {
+        $token = Configure::read('Settings.Cms.SEO.google-site-verification');
+        if (empty($token)) {
+            return '';
+        }
+        return $this->_render(self::ATTR_NAME, 'google-site-verification', $token);
+    }
+
+    /**
+     * Render the google nositelinkssearchbox meta tag to not display a search box beneath the
+     * search engine result for your site.
+     *
+     * @return string
+     */
+    public function displaySearchBox()
+    {
+        $displaySearchBox = (bool)Configure::read('Settings.Cms.SEO.display-search-box');
+        if ($displaySearchBox) {
+            return '';
+        }
+        return $this->_render(self::ATTR_NAME, 'google', 'nositelinkssearchbox');
+    }
+
+    /**
+     * Render the ld+json script tag to set the schema.org context for a page.
+     *
+     * @return string
+     */
+    public function ldJson()
+    {
+        $attributes = [
+            '@context' => 'http://schema.org',
+            '@type' => 'WebSite',
+            'url' => $this->Url->build('/', true),
+            'potentialAction' => [
+                '@type' => 'SearchAction',
+                'query-input' => 'required name=search_term_string',
+                'target' => $this->Url->build([
+                    'plugin' => 'Wasabi/Cms',
+                    'controller' => 'Frontend/Search',
+                    'action' => 'search',
+                    'language_id' => Wasabi::contentLanguage()->id
+                ], true) . '?q={search_term_string}'
+            ]
+        ];
+
+        if (!empty(WasabiCms::$instanceName)) {
+            $attributes['name'] = WasabiCms::$instanceName;
+        }
+
+        $sameAsKeys = [
+            'Settings.Cms.SEO.Social.facebook_url',
+            'Settings.Cms.SEO.Social.instagram_url',
+            'Settings.Cms.SEO.Social.linkedin_url',
+            'Settings.Cms.SEO.Social.myspace_url',
+            'Settings.Cms.SEO.Social.pinterest_url',
+            'Settings.Cms.SEO.Social.youtube_url',
+            'Settings.Cms.SEO.Social.googleplus_url'
+        ];
+        $sameAs = [];
+
+        foreach ($sameAsKeys as $key) {
+            $setting = Configure::read($key);
+            if (!empty($setting)) {
+                $sameAs[] = $setting;
+            }
+        }
+
+        $twitterName = Configure::read('Settings.Cms.SEO.Social.twitter_username');
+        if (!empty($twitterName)) {
+            $sameAs[] = 'https://twitter.com/' . $twitterName;
+        }
+
+        if (!empty($sameAs)) {
+            $attributes['sameAs'] = $sameAs;
+        }
+
+        return '<script type="application/ld+json">' . json_encode($attributes) . '</script>';
+    }
+
+    /**
+     * Render a meta tag with the given $attr name, $property and its $content.
+     *
+     * @param string $attr The attribute, e.g. name or description.
+     * @param string $property The property name.
+     * @param string $content The property content.
+     * @param bool|string $secAttr
+     * @param bool|string $secProp
+     * @return string
+     */
+    protected function _render($attr, $property, $content, $secAttr = false, $secProp = false)
+    {
+        if (empty($content)) {
+            return '';
+        }
+        $secOutput = '';
+        if ($secAttr !== false && $secProp !== false) {
+            $secOutput = ' ' . $secAttr . '="' . $secProp . '"';
+        }
+        return '<meta ' . $attr . '="' . $property . '"' . $secOutput . ' content="' . $content . '">';
+    }
+
+    /**
+     * Get the title for the given $entity.
+     *
+     * @param PublishableEntity $entity The entity, e.g. Page or Post.
+     * @return string
+     */
+    protected function _getTitle($entity)
+    {
+        $title = $entity->page_title;
+        if ((bool)$entity->display_page_title_suffix) {
+            $title .= WasabiCms::titleSuffix();
+        }
+        return $title;
+    }
+
+    /**
+     * Get the social image for the given $entity.
+     *
+     * @param PublishableEntity $entity The entity, e.g. Page or Post.
+     * @return string
+     */
+    protected function _getSocialImage($entity)
+    {
+    }
 }
